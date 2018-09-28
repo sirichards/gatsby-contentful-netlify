72610fe1340854dc314d17e953a7d3ca45208d15
Simon Richards <s.richards@dewynters.com>
2018-09-28 15:43:26 +0100

    first commit

---
+++ gatsby-config.js
@@ -0,0 +1,28 @@
+module.exports = {
+	siteMetadata: {
+		title: 'Contentful + Gatsby',
+	},
+	plugins: [
+		'gatsby-plugin-react-helmet',
+		{
+			resolve: `gatsby-source-contentful`,
+			options: {
+				spaceId: `zay2hx1t09mg`,
+				accessToken: `4777fc5d724e1378a0c6f7de5cae1d26fd5de094941fb0b6cf516fe71d213ea9`,
+			},
+		},
+		{
+			resolve: `gatsby-plugin-manifest`,
+			options: {
+				name: 'gatsby-starter-default',
+				short_name: 'starter',
+				start_url: '/',
+				background_color: '#663399',
+				theme_color: '#663399',
+				display: 'minimal-ui',
+				icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
+			},
+		},
+		'gatsby-plugin-offline',
+	],
+}
---
+++ src/pages/index.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+import Layout from '../components/layout'
+
+const IndexPage = ({data}) => (
+	<Layout>
+		<h1>Hi people</h1>
+		<p>Welcome to your new Gatsby site.</p>
+		<p>Now go build something great.</p>
+		<Link to="/page-2/">Go to page 2</Link>
+		{data.allContentfulBlogPost.edges.map(({ node }, index) => (
+			<h2 key={index}>{node.title} / {node.slug}</h2>
+		))}
+	</Layout>
+)
+	
+export default IndexPage
+
+export const query = graphql`
+query {
+	allContentfulBlogPost {
+		edges {
+			node {
+				title
+				slug
+			}
+		}
+	}
+}
+`
