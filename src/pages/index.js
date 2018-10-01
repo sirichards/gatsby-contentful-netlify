f1204e993ec78dc1e388137f6fe8698f1c34b413
Simon Richards <s.richards@dewynters.com>
2018-10-01 12:54:35 +0100

    delete line

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,6 @@ const IndexPage = ({data}) => (
 	<Layout>
 		<h1>Hi people</h1>
 		<p>Welcome to your new Gatsby site.</p>
-		<p>Now go build something great.</p>
 		<Link to="/page-2/">Go to page 2</Link>
 		{data.allContentfulBlogPost.edges.map(({ node }, index) => (
 			<h2 key={index}>{node.title} / {node.slug}</h2>
